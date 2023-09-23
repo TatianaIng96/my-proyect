d70510bb1fdd37234d6c3e8635b019c80e24f8ad
Tatiana Cardona <clleidytatina@gmail.com>
2023-09-22 21:58:10 -0500

    home landding page

---
+++ src/api/booky/controllers/booky.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * booky controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::booky.booky');
---
+++ src/api/booky/routes/booky.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * booky router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::booky.booky');
